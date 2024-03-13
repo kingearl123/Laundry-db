73b26cb99e55c23d05bda0097ed96a887803d540
kingearl123 <100173073+kingearl123@users.noreply.github.com>
2024-03-13 10:47:39 +0800

    Update home.php

--- homepage/home.php
+++ homepage/home.php
@@ -1,4 +1,55 @@
 <section class="main">
-        <h2>Dashboard</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at libero justo, eget egestas velit. Sed nec nibh velit. Sed at libero justo, eget egestas velit. Sed nec nibh velit.</p>
+<div class="container">
+    <h2 class="title">Dashboard</h2>
+    <div class="grid">
+        <!-- Card 1 -->
+        <div class="card">
+            <div class="card-content">
+            <?php
+                $acsql = "SELECT * from tb_transaksi";
+                if ($sho = mysqli_query($conn, $acsql)) {
+                    $jumla = mysqli_num_rows($sho);
+                ?>
+                <dl>
+                    <dt class="description">Total Transaksi</dt>
+                    <dd class="value"><?php printf(" %d\n", $jumla) ?></dd>
+                </dl>
+                <?php }?>
+
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-content">
+            <?php
+                $acsql1 = "SELECT * from tb_paket";
+                if ($sho = mysqli_query($conn, $acsql1)) {
+                    $jumla = mysqli_num_rows($sho);
+                ?>
+                <dl>
+                    <dt class="description">Total Paket</dt>
+                    <dd class="value"><?php printf(" %d\n", $jumla) ?></dd>
+                </dl>
+                <?php }?>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-content">
+            <?php
+                $acsql2 = "SELECT * from tb_member";
+                if ($sho = mysqli_query($conn, $acsql2)) {
+                    $jumla = mysqli_num_rows($sho);
+                ?>
+                <dl>
+                    <dt class="description">Total Member</dt>
+                    <dd class="value"><?php printf(" %d\n", $jumla) ?></dd>
+                </dl>
+                <?php }?>
+            </div>
+        </div>
+
+    </div>
+</div>
+
 </section>
